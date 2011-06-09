df74354e5b73e5c59c35f07ccb271a28eaec475c
Christian Richards <cjrgaming@gmail.com>
2011-06-09 15:51:47 -0700

    Competed Blank Server and Basic Client

---
+++ AegisBorn3dPhoton/Assets/_Scripts/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ExitGames.Client.Photon;
+
+public class Game : IPhotonPeerListener
+{
+
+    PhotonPeer peer;
+
+    public string status = "disconnected";
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+    }
+
+    public void EventAction(byte eventCode, System.Collections.Hashtable photonEvent)
+    {
+    }
+
+    public void OperationResult(byte opCode, int returnCode, System.Collections.Hashtable returnValues, short invocID)
+    {
+    }
+
+    public void PeerStatusCallback(StatusCode statusCode)
+    {
+        switch (statusCode)
+        {
+            case StatusCode.Connect:
+                {
+                    status = "connected";
+                    break;
+                }
+
+            case StatusCode.Disconnect:
+            case StatusCode.DisconnectByServer:
+            case StatusCode.DisconnectByServerLogic:
+            case StatusCode.DisconnectByServerUserLimit:
+            case StatusCode.TimeoutDisconnect:
+                {
+                    status = "disconnected";
+                    break;
+                }
+
+            default:
+                {
+                    status = "unexpected";
+                    break;
+                }
+        }
+    }
+
+    public void Initialize(PhotonPeer peer, string serverAddress, string applicationName)
+    {
+        this.peer = peer;
+        peer.Connect(serverAddress, applicationName);
+    }
+
+    public void Disconnect()
+    {
+        peer.Disconnect();
+    }
+
+    public void Update()
+    {
+        peer.Service();
+    }
+}
--- AegisBorn3dPhoton/Assets/_Scripts/Login.cs
+++ AegisBorn3dPhoton/Assets/_Scripts/Login.cs
@@ -1,15 +1,58 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using ExitGames.Client.Photon;
 
-public class Login : MonoBehaviour {
+public class Login : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Game engine;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+        engine = new Game();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            this.engine.Update();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    /// <summary>
+    /// The on application quit.
+    /// </summary>
+    public void OnApplicationQuit()
+    {
+        try
+        {
+            this.engine.Disconnect();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 60, 100, 30), "Connect"))
+        {
+            PhotonPeer peer = new PhotonPeer(this.engine, false);
+
+            engine.Initialize(peer, "localhost:5055", "AegisBorn");
+        }
+        GUI.Label(new Rect(100, 100, 300, 300), engine.status);
+    }
 }
